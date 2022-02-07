3a483f6103ade4b0d09e965633b2153def163830
bistty <bisolajesukunle@gmail.com>
2022-02-07 22:30:34 +0100

    Added some new features

--- script.js
+++ script.js
@@ -1,93 +1,93 @@
-window.addEventListener("DOMContentLoaded", function () {
-  orderButtons = document.querySelectorAll("button[data-order]");
+window.addEventListener('DOMContentLoaded', function () {
+  orderButtons = document.querySelectorAll('button[data-order]');
 
   let order;
   orderButtons.forEach((button) => {
-    button.addEventListener("click", function (e) {
+    button.addEventListener('click', function (e) {
       const button = e.currentTarget;
       const container = button.parentNode;
 
       order = {
-        id: button.getAttribute("data-order"),
-        title: container.querySelector(".title").innerText,
-        price: container.querySelector(".price").innerText,
-        desc: container.querySelector(".desc").innerText,
+        id: button.getAttribute('data-order'),
+        title: container.querySelector('.title').innerText,
+        price: container.querySelector('.price').innerText,
+        desc: container.querySelector('.desc').innerText,
       };
       console.log(order.id, order.title, order.price, order.desc);
-      localStorage.setItem("order", JSON.stringify(order));
+      localStorage.setItem('order', JSON.stringify(order));
 
-      const url = window.location.href.replace("pies.html", "order.html");
+      const url = window.location.href.replace('pies.html', 'order.html');
       window.location.href = url;
     });
   });
 
-  order = localStorage.getItem("order");
+  order = localStorage.getItem('order');
 
   if (order) {
     const pieOrder = JSON.parse(order);
 
-    const pieContainer = document.querySelector(".pie");
+    const pieContainer = document.querySelector('.pie');
 
-    const title = pieContainer.querySelector(".title");
+    const title = pieContainer.querySelector('.title');
 
-    const price = pieContainer.querySelector(".price");
+    const price = pieContainer.querySelector('.price');
 
-    const desc = pieContainer.querySelector(".desc");
+    const desc = pieContainer.querySelector('.desc');
 
-    const img = pieContainer.querySelector("img");
+    const img = pieContainer.querySelector('img');
 
     title.innerText = pieOrder.title;
     price.innerText = pieOrder.price;
     desc.innerText = pieOrder.desc;
-    img.setAttribute("src", `./images/${pieOrder.id}.png`);
+    img.setAttribute('src', `./images/${pieOrder.id}.png`);
   }
 
   // form
 
   //firebase
   var firebaseConfig = {
-    apiKey: "AIzaSyAobJdtgh2eRPSxS7-pczCS8MpT6kf71Nk",
-    authDomain: "bethany-s-pie.firebaseapp.com",
-    databaseURL: "https://bethany-s-pie-default-rtdb.firebaseio.com",
-    projectId: "bethany-s-pie",
-    storageBucket: "bethany-s-pie.appspot.com",
-    messagingSenderId: "999401576028",
-    appId: "1:999401576028:web:845363cd6428ebb262f899",
-    measurementId: "G-1YZJ021HPX",
+    apiKey: 'AIzaSyAobJdtgh2eRPSxS7-pczCS8MpT6kf71Nk',
+    authDomain: 'bethany-s-pie.firebaseapp.com',
+    databaseURL: 'https://bethany-s-pie-default-rtdb.firebaseio.com',
+    projectId: 'bethany-s-pie',
+    storageBucket: 'bethany-s-pie.appspot.com',
+    messagingSenderId: '999401576028',
+    appId: '1:999401576028:web:845363cd6428ebb262f899',
+    measurementId: 'G-1YZJ021HPX',
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
-  var messageRef = firebase.database().ref("messages");
+  var messageRef = firebase.database().ref('messages');
 
   const contactForm = document
-    .getElementById("contactForm")
-    .addEventListener("submit", function (e) {
+    .getElementById('contactForm')
+    .addEventListener('submit', function (e) {
       e.preventDefault();
 
       const form = {
-        firstName: getInputValues("first-name"),
-        lastName: getInputValues("last-name"),
-        address: getInputValues("address"),
-        state: getInputValues("state"),
-        postalCode: getInputValues("postal-code"),
-        comments: getInputValues("comments"),
+        firstName: getInputValues('first-name'),
+        lastName: getInputValues('last-name'),
+        address: getInputValues('address'),
+        state: getInputValues('state'),
+        postalCode: getInputValues('postal-code'),
+        comments: getInputValues('comments'),
         orders: JSON.parse(order),
       };
       saveMessages(form);
-      document.getElementById("contactForm").reset();
+      document.getElementById('contactForm').reset();
       //show alert
 
-      const modal = document.querySelector(".modal");
-      const overlay = document.querySelector(".overlay");
+      const modal = document.querySelector('.modal');
+      const overlay = document.querySelector('.overlay');
 
-      modal.classList.remove("hidden");
-      overlay.classList.remove("hidden");
+      modal.classList.remove('hidden');
+      overlay.classList.remove('hidden');
 
       setTimeout(() => {
-        modal.classList.add("hidden");
-        overlay.classList.add("hidden");
-        const url = window.location.href.replace("order.html", "pies.html");
+        modal.classList.add('hidden');
+        overlay.classList.add('hidden');
+        const url = window.location.href.replace('order.html', 'pies.html');
         window.location.href = url;
       }, 3000);
     });
@@ -100,8 +100,4 @@ window.addEventListener("DOMContentLoaded", function () {
     var newMessageRef = messageRef.push();
     newMessageRef.set(form);
   }
-
-  const openModal = function () {};
-
-  const closeModal = function () {};
 });
